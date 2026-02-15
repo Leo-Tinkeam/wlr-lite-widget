9beaf3e3b4232ecb37e09a36d012fa2158440cca
Léo-Tinkeam <sarcyleo@gmail.com>
2026-02-15 21:27:40 +0100

    Rename Layer to Widget (file)

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,8 @@
-mod layer;
+mod widget;
 mod settings;
 
-pub use layer::Widget;
+pub use widget::Widget;
 pub use settings::{SizeUnit, WidgetAnchor, WidgetMargin, WidgetPosition, WidgetSize};
 pub use smithay_client_toolkit::shell::wlr_layer::{Anchor, Layer};
 
 pub(crate) use settings::Margin;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
-    }
-}
